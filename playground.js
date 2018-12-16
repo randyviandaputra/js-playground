ed946f6cd7731721be63521998939686844e9f69
Randy Vianda Putra <modulloe@gmail.com>
2018-12-16 18:21:28 +0700

    refactor to async await

--- playground.js
+++ playground.js
@@ -5,17 +5,19 @@ button.addEventListener('click', (event) => {
     comments.insertAdjacentHTML('beforeend', `<p class="comment">${commentContent}</p>`);
 });
 
-fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json())
-    .then((data) => {
-        const users = document.getElementById('list-user');
-        data.forEach((val, key) => {
-            const html = `
-                <h5>Nama : ${val.name}</h5>
-                <h5>Email : ${val.email}</h5>
-                <h5>Company : ${val.company.name}</h5>
-                <p>Alamat : ${val.address.street}, ${val.address.city}</p>
-            `;
-            users.insertAdjacentHTML('beforeend', `<div class="card">${html}</div>`);
-        })
-    });
+async function getUser() {
+    let response = await fetch("https://jsonplaceholder.typicode.com/users");
+    let data = await response.json();
+    const users = document.getElementById('list-user');
+    data.forEach((val, key) => {
+        const html = `
+            <h5>Nama : ${val.name}</h5>
+            <h5>Email : ${val.email}</h5>
+            <h5>Company : ${val.company.name}</h5>
+            <p>Alamat : ${val.address.street}, ${val.address.city}</p>
+        `;
+        users.insertAdjacentHTML('beforeend', `<div class="card">${html}</div>`);
+    })
+}
+
+getUser();
